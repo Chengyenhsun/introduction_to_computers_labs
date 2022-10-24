5e4b3f01e3bb335360cadbd34394d8e706fd8822
Chengyenhsun <f14106155@gs.ncku.edu.tw>
2022-10-24 05:34:13 +0100

    seven

--- lab07/lab07_plus.py
+++ lab07/lab07_plus.py
@@ -34,26 +34,26 @@ class Dogs(Animal):  #定義子類別
         Dogs.bath(self, n_bath)
         print("狗狗現在的體重=", self.weight ,"kg,", "心情", self.mood)  #輸出
 
-class Shiba(Dogs):
-    def __init__(self, weight, mood):
-        super().__init__(weight, mood)
+class Shiba(Dogs):  #定義子類別Shiba為Dogs的一種
+    def __init__(self, weight, mood):  #建構式
+        super().__init__(weight, mood)  #執行父類別的方法
     
     def feed(self, n_feed):
         self.weight = self.weight + n_feed*0.3
         self.mood = self.mood + n_feed*5
     
-    def printf(self, n_feed, n_walk, n_bath):
-        Shiba.feed(self, n_feed)
+    def printf(self, n_feed, n_walk, n_bath):  #定義輸出次數方法
+        Shiba.feed(self, n_feed)  #呼叫子類別方法
         Shiba.walk(self, n_walk)
         Shiba.bath(self, n_bath)
         print("柴犬現在的體重=", self.weight ,"kg,", "心情", self.mood)  #輸出
 
-    def mood_constraint(self, constraint):
+    def mood_constraint(self, constraint):  #定義mood上限
         self.constr = constraint
         if(90<=constraint<300):
                 self.mood = self.constr
-                print("所以，柴犬現在的心情", self.mood)
-        print("mood 最高只能為=", self.constr)
+                print("所以，柴犬現在的心情", self.mood)  #輸出
+        print("mood 最高只能為=", self.constr)  #輸出
     
 
 shiba = Shiba(5, 70) 
